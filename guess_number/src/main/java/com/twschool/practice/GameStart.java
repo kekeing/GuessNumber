837b7e69d5400dae16f806d795abc8de65214b24
kekeing <851575436@qq.com>
2020-06-16 21:45:46 +0800

    add by like complete main and set standardAnswer is a constant and without judge input element

--- guess_number/src/main/java/com/twschool/practice/GameStart.java
+++ guess_number/src/main/java/com/twschool/practice/GameStart.java
@@ -4,18 +4,30 @@ import java.util.Scanner;
 
 public class GameStart {
     public static void main(String[] args) {
-        int i = 0;
+        HandleUserAnswer handleUserAnswer = new HandleUserAnswer();
+        GameResultInfo gameResultInfo = new GameResultInfo();
+        HandleGameOver handleGameOver = new HandleGameOver();
+        String[] standardAnswer = {"1","2","3","4"};
+        int remainedChances = 6;
         Scanner scanner = new Scanner(System.in);
-        while (i < 1) {
+        System.out.println("Game Start");
+        System.out.println("please input four number between 0-9 and split with ','");
+        while (remainedChances > 0) {
             if (scanner.hasNext()) {
                 String inputString = scanner.nextLine();
-                System.out.println(inputString);
                 String[] userAnswer = inputString.split(",");
-                for (String userAnswerString : userAnswer) {
-                    System.out.println(userAnswerString);
+                String handleUserAnswerResult =handleUserAnswer.handleUserAnswerStandardAnswer(userAnswer,standardAnswer);
+                System.out.println(handleUserAnswerResult);
+                gameResultInfo = handleGameOver.getGameResultInfo(handleUserAnswerResult,remainedChances);
+                if (gameResultInfo.getOver() && "Success".equals(gameResultInfo.getDescription())){
+                    System.out.println("You win!!!");
+                    break;
                 }
             }
+            remainedChances--;
         }
+        if (remainedChances == 0 && "Lose".equals(gameResultInfo.getDescription())){
+        System.out.println("You lose!!!");}
         scanner.close();
     }
 }
